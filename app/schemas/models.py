7c1e00aee267d1b5f7c7b8ec2dce9d0e643e86df
Alonso Navas <miguel.alonso.navas.leon.ravinez@linkhub.ia>
2026-04-29 14:18:05 -0500

    Tipo de datos de modelps pydantic modificados

--- app/schemas/models.py
+++ app/schemas/models.py
@@ -5,9 +5,9 @@ from pydantic import BaseModel, Field, EmailStr
 class User(BaseModel):
     nombre: str = Field(..., min_length=1)
     apellido: str = Field(..., min_length=1)
-    dni: int = Field(..., gt=0, lt=100000000)
-    telefono: int = Field(..., gt=0, lt=1000000000)
-    contraseña: str = Field(..., min_length=6)
+    dni: str = Field(..., min_length=1, max_length=8, pattern=r'^\d+$')
+    telefono: str = Field(..., min_length=1, max_length=9, pattern=r'^\d+$')
+    password: str = Field(..., min_length=6)
 
 
 class UserResponse(BaseModel):
